ff0c00540af9c8a5ea468be387314ce6277cbd2e
Love2013 <17320066637@163.com>
2024-07-13 16:20:15 +0800

    git

--- log.h
+++ log.h
@@ -18,17 +18,26 @@
 #include <iostream>
 
 namespace  zero {
+class Logger;
 class LogEvent {
 public:
     using logEvent_ptr = std::shared_ptr<LogEvent>;
     LogEvent();
+
+    const char* getFile() const { return m_file; }
+    int32_t getLine() const { return m_line;}
+    uint32_t getElapse() const {return m_elapse;}
+    uint32_t getThreadId() const { return m_threadId;}
+    uint32_t getFiberId() const { return m_fiberId;}
+    uint64_t getTime() const { return m_time;}
+    const std::string& getContent() const { return m_content; }
 private:
     const char* m_file = nullptr;  /* 文件名 */
     int32_t m_line = 0;            /* 行号 */
     uint32_t m_threadId = 0;       /* 线程id */
     uint32_t m_fiberId = 0;        /* 协程id*/
     uint64_t m_time;               /* 时间戳*/
-    uint32_t elapse = 0;           /* 程序启动开始到现在的毫秒数*/
+    uint32_t m_elapse = 0;           /* 程序启动开始到现在的毫秒数*/
     std::string m_content;         
 
 };
@@ -37,22 +46,38 @@ private:
 class LogLevel {
 public:
     enum Level {
+        UNKNOW = 0,
         DEBUG = 1,
         INFO  = 2,
         WARN  = 3,
         ERROR = 4,
         FATAL = 5
     };
+    static const char* ToString(LogLevel::Level level);
+
 };
 
 /* LogFormat（日志格式器） */
 class LogFormatter {
 public:
     using logFormatter_ptr = std::shared_ptr<LogFormatter>;
+    LogFormatter(const std::string& pattern);
+    /* %t %thread_id %m%n */
+    std::string format(std::shared_ptr<Logger> logger_ptr, LogLevel::Level level,LogEvent::logEvent_ptr event);
+public:
+    class FormatItem {
+        public:
+            using formatItem_ptr = std::shared_ptr<FormatItem>;
+            virtual ~FormatItem() {}
+            /* 将对于的日志格式内容写入到os*/
+            virtual void format(std::ostream& os,std::shared_ptr<Logger> logger_ptr, LogLevel::Level level, LogEvent::logEvent_ptr event) = 0;
 
-protected:
-    std::string format(LogEvent::logEvent_ptr event);
-
+        private:  
+    };
+    void init();
+private:
+    std::string m_pattern;
+    std::vector<FormatItem::formatItem_ptr> m_formatItems;
 };
 
 /* LogAppender（输出落地点） */
@@ -61,14 +86,13 @@ public:
     using logAppender_ptr = std::shared_ptr<LogAppender>;
     virtual ~LogAppender() { };
 
-    virtual void log(LogLevel::Level level,LogEvent::logEvent_ptr event) = 0;
+    virtual void log(std::shared_ptr<Logger> logger_ptr, LogLevel::Level level,LogEvent::logEvent_ptr event) = 0;
     void setFormatter(LogFormatter::logFormatter_ptr format) {  m_formatter = format; }
     LogFormatter::logFormatter_ptr getFormatter() const { return m_formatter; }
 private:
 protected:
     LogLevel::Level m_level;
-    /
-    LogFormatter::logFormatter_ptr m_formatter;
+    LogFormatter::logFormatter_ptr m_formatter;                      /* 日志格式包装器 */   
 };
 
 /* 将日志抽象成Logger（日志器）*/
@@ -98,7 +122,7 @@ private:
 class StdoutLogAppender : public LogAppender {
 public:
     using stdoutLogAppender_ptr = std::shared_ptr<StdoutLogAppender>;
-    virtual void log(LogLevel::Level level,LogEvent::logEvent_ptr event) override;
+    virtual void log(std::shared_ptr<Logger> logger_ptr,LogLevel::Level level,LogEvent::logEvent_ptr event) override;
 };
 
 /* 定义输出到文件的appender */
@@ -106,10 +130,10 @@ class FileLogAppender : public LogAppender {
 public:
     using fileLogAppender_ptr = std::shared_ptr<FileLogAppender>;
     FileLogAppender(const std::string& filename);
-    virtual void log(LogLevel::Level level,LogEvent::logEvent_ptr event) override;
-    void reopen();                                                     /* 文件打开 */
+    virtual void log(std::shared_ptr<Logger> logger_ptr, LogLevel::Level level,LogEvent::logEvent_ptr event) override;
+    bool reopen();                                                     /* 文件打开 */
 private:
-    std::string m_name;
+    std::string m_filename;
     std::ofstream m_filestream;
 };
 
