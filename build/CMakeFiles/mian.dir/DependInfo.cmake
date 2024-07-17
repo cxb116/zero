f32672d40a30bdbe14f00766c94cb5654b2a8647
Love2013 <17320066637@163.com>
2024-07-17 10:54:32 +0800

    test

---
+++ build/CMakeFiles/mian.dir/DependInfo.cmake
@@ -0,0 +1,36 @@
+
+# Consider dependencies only in project.
+set(CMAKE_DEPENDS_IN_PROJECT_ONLY OFF)
+
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  )
+
+# The set of dependency files which are needed:
+set(CMAKE_DEPENDS_DEPENDENCY_FILES
+  "/project/zero/Acceptor.cc" "CMakeFiles/mian.dir/Acceptor.cc.o" "gcc" "CMakeFiles/mian.dir/Acceptor.cc.o.d"
+  "/project/zero/Buffer.cc" "CMakeFiles/mian.dir/Buffer.cc.o" "gcc" "CMakeFiles/mian.dir/Buffer.cc.o.d"
+  "/project/zero/Channel.cc" "CMakeFiles/mian.dir/Channel.cc.o" "gcc" "CMakeFiles/mian.dir/Channel.cc.o.d"
+  "/project/zero/CurrentThread.cc" "CMakeFiles/mian.dir/CurrentThread.cc.o" "gcc" "CMakeFiles/mian.dir/CurrentThread.cc.o.d"
+  "/project/zero/DefaultPoller.cc" "CMakeFiles/mian.dir/DefaultPoller.cc.o" "gcc" "CMakeFiles/mian.dir/DefaultPoller.cc.o.d"
+  "/project/zero/EPollPoller.cc" "CMakeFiles/mian.dir/EPollPoller.cc.o" "gcc" "CMakeFiles/mian.dir/EPollPoller.cc.o.d"
+  "/project/zero/EventLoop.cc" "CMakeFiles/mian.dir/EventLoop.cc.o" "gcc" "CMakeFiles/mian.dir/EventLoop.cc.o.d"
+  "/project/zero/EventLoopThread.cc" "CMakeFiles/mian.dir/EventLoopThread.cc.o" "gcc" "CMakeFiles/mian.dir/EventLoopThread.cc.o.d"
+  "/project/zero/EventLoopThreadPool.cc" "CMakeFiles/mian.dir/EventLoopThreadPool.cc.o" "gcc" "CMakeFiles/mian.dir/EventLoopThreadPool.cc.o.d"
+  "/project/zero/InetAddress.cc" "CMakeFiles/mian.dir/InetAddress.cc.o" "gcc" "CMakeFiles/mian.dir/InetAddress.cc.o.d"
+  "/project/zero/Logger.cc" "CMakeFiles/mian.dir/Logger.cc.o" "gcc" "CMakeFiles/mian.dir/Logger.cc.o.d"
+  "/project/zero/Poller.cc" "CMakeFiles/mian.dir/Poller.cc.o" "gcc" "CMakeFiles/mian.dir/Poller.cc.o.d"
+  "/project/zero/Socket.cc" "CMakeFiles/mian.dir/Socket.cc.o" "gcc" "CMakeFiles/mian.dir/Socket.cc.o.d"
+  "/project/zero/TcpConnection.cc" "CMakeFiles/mian.dir/TcpConnection.cc.o" "gcc" "CMakeFiles/mian.dir/TcpConnection.cc.o.d"
+  "/project/zero/TcpServer.cc" "CMakeFiles/mian.dir/TcpServer.cc.o" "gcc" "CMakeFiles/mian.dir/TcpServer.cc.o.d"
+  "/project/zero/Thread.cc" "CMakeFiles/mian.dir/Thread.cc.o" "gcc" "CMakeFiles/mian.dir/Thread.cc.o.d"
+  "/project/zero/Timestamp.cc" "CMakeFiles/mian.dir/Timestamp.cc.o" "gcc" "CMakeFiles/mian.dir/Timestamp.cc.o.d"
+  "/project/zero/main.cc" "CMakeFiles/mian.dir/main.cc.o" "gcc" "CMakeFiles/mian.dir/main.cc.o.d"
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
---
+++ build/CMakeFiles/mian.dir/depend.make
@@ -0,0 +1,2 @@
+# Empty dependencies file for mian.
+# This may be replaced when dependencies are built.
