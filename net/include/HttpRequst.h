9e5cc886cd7a6fd244d103c866310e58b83309b2
Love2013 <17320066637@163.com>
2024-07-23 11:10:46 +0800

    tijiao

--- net/include/HttpRequst.h
+++ net/include/HttpRequst.h
@@ -89,13 +89,60 @@ const char* methodString() const {
         query_.assign(start,end);
     }
 
+    const std::string& query() const {
+        return query_;
+    }
+
+    void setReceiveTime(Timestamp t) {
+        receiveTime_  = t;
+    }
+
+    Timestamp receiveTime() const {
+        return receiveTime_;
+    }
+    /* request header spliter */
+    void addHeader(const char* start,const char* colon, const char* end) {
+        std::string field(start,colon);
+        ++colon;
+        while(colon < end && isspace(*colon)) {
+            ++colon;
+        }
+        std::string value(colon,end);
+        while(!value.empty() && isspace(value[value.size()-1])) {
+            value.resize(value.size()-1);
+        }
+        headers_[field] = value;
+    }
+    /* 根据 header key return value*/
+    std::string getHeader(const std::string& field) const{
+        std::string result;
+        std::map<std::string,std::string>::const_iterator it = headers_.find(field);
+        if(it !=headers_.end()) {
+            result = it->second;
+        }
+        return result;
+    }
+
+    const std::map<std::string,std::string>& headers() const {
+        return headers_;
+    }
+
+    void swap(HttpRequest& that) {
+        std::swap(method_,that.method_);
+        std::swap(version_,that.version_);
+        path_.swap(that.path_);
+        query_.swap(that.query_);
+        receiveTime_.swap(that.receiveTime_);
+        headers_.swap(that.headers_);
+    }
+
 
 private:
     Method method_;
     Version version_;
     std::string path_;
-    std::string query_;
-    Timestamp receiveTime_;
+    std::string query_;                     
+    Timestamp receiveTime_;             /* request time */
     std::map<std::string,std::string> headers_;
 };
 
